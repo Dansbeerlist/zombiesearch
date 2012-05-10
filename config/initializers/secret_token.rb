c4eca8c5f40da286589b82402790faa94d3c8e1c
Dan Warter <dan@dansbeerlist.com>
2012-05-10 10:48:00 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zombiesearch::Application.config.secret_token = 'f526344f7e27ca810da4eb70b6386080be7feedd6432326fff6a37972d2901492ac985aa149293ca0e4bad2e9f0d5d6441d56f010cf9af5df7d4231e1e26db86'
